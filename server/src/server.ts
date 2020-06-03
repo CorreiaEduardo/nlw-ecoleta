c907d23d6d51606f6200f0025259ebd3863c2cd4
Eduardo Correia <correiaeduardojr@gmail.com>
2020-06-02 23:50:08 -0300

    💄✨ add linting utilities

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,12 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
-app.get("/hello", (_, res) => {
-    res.json({message: 'Hello World!'});
+app.get('/hello', (_, res) => {
+  res.json({ message: 'Hello World!' });
 });
 
 const port = 3333;
-
 app.listen(port, () => {
-    console.log(`Starting nlw-ecoleta-server on port ${port}`)
-})
+  console.log(`Starting nlw-ecoleta-server on port ${port}`);
+});
