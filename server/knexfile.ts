614cf9ee89756f2bd4f998bcd1963b7946f5623a
Eduardo Correia <correiaeduardojr@gmail.com>
2020-06-07 18:35:11 -0300

    🐛 fix: environment variable for connection string

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -2,7 +2,7 @@ import path from 'path';
 
 module.exports = {
   client: process.env.KNEX_CLIENT,
-  connection: process.env.PG_CONNECTION_STRING,
+  connection: process.env.CONNECTION_STRING,
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
   },
--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -2,7 +2,7 @@ import knex from 'knex';
 
 const connection = knex({
   client: process.env.KNEX_CLIENT,
-  connection: process.env.PG_CONNECTION_STRING,
+  connection: process.env.CONNECTION_STRING,
 });
 
 export default connection;
